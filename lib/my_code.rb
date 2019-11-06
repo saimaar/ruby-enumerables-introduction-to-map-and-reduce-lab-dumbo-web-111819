32a204030b13b7b17670cfc546f93403ea2bcc96
Saima Rahman <rahman.saima92@gmail.com>
2019-11-06 00:13:36 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -45,7 +45,7 @@ def map_to_square(source_array)
 end 
 
 
-def reduce_to_total(source_array)
+def reduce_to_total(source_array, starting_point = 0)
   total = 0 
   i = 0 
   while i < source_array.length do 
