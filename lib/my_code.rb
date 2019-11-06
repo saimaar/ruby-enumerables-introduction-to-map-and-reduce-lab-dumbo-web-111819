7b435cca5b6f1ef0c1e124f10a6fe598b24f6f6f
Saima Rahman <rahman.saima92@gmail.com>
2019-11-06 00:18:41 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -57,5 +57,8 @@ def reduce_to_total(source_array, starting_point = 0)
 end 
 
 def reduce_to_all_true(source_array)
-  
+  i = 0 
+  while i < source_array.length do 
+    num = source_array[i]
+  end 
 end 
